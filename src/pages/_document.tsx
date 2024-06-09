e7f8e135e30caf195c4b2a0c595f719e13302f2c
Rafael Figueiredo <60237326+RafaelFigueiredo2203@users.noreply.github.com>
2024-06-09 12:16:34 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
        <Head>
+         <meta name="google-site-verification" content="9eCg0sBmScwH139o1Qp4KlkvKrULwLLFNsER7Zhe4N8" />
         <title>Gerador de QR Code</title>
         <meta name="description" content="Gere seus QR codes personalizados facilmente com nosso gerador de QR Code." />
       </Head>
